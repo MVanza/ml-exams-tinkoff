434882ec5db92803968c4265626423790fa91976
MVanza <68096976+MVanza@users.noreply.github.com>
2021-01-17 23:42:16 +0300

    sudoku with player
    sudoku with player almost done. save doesn't work, trying to fix it

--- sudoku.py
+++ sudoku.py
@@ -68,19 +68,19 @@ def get_random_square(field, square_num):
 
 
 class Sudoku:
-    def __init__(self, mode, field_size):
-        self.mode = mode
-        self.field = self.make_field(field_size)
 
     @staticmethod
     def make_field(field_size):
-        base_field = [[((i*3 + i/3 + j) % (3*3) + 1) for j in range(3*3)] for i in range(3*3)]
+        base_field = [[int(((i*3 + i/3 + j) % (3*3) + 1)) for j in range(3*3)] for i in range(3*3)]
         shuffle_func = ['transpose(base_field)', 'swap_rows_small(base_field)',
                         'swap_columns_small(base_field)', 'swap_rows_area(base_field)',
                         'swap_columns_area(base_field)']
+        # перемещаем нашу таблицу 10 раз
         for _ in range(10):
             base_field = eval(shuffle_func[random.randrange(0, len(shuffle_func), 1)])
-        new_field = get_random_square(base_field, 81 - field_size)
+
+        # удаляем случайные клетки
+        new_field = get_random_square(base_field, field_size)
         return new_field
 
 
@@ -88,6 +88,7 @@ class AISudoku(Sudoku):
     """
     Класс для игры с компьютером
     """
+
     pass
 
 
@@ -95,11 +96,56 @@ class PlayerSudoku(Sudoku):
     """
     Класс для игры с человеком
     """
-    def save_field(self):
-        pass
+    ans = []
+
+    def __init__(self, field_size):
+        self.field_size = field_size
+        self.field = []
+        self.download_field()
+
+    def save(self):
+        with open('data.pkl', 'wb') as file:
+            pickle.dump(self.field, file)
+
+    def __del__(self):
+        self.save()
 
     def download_field(self):
-        pass
+        try:
+            f = open('data.pkl', 'rb')
+            if pickle.load(f) != '':
+                self.field = pickle.load(f)
+            else:
+                self.field = self.make_field(self.field_size)
+            f.close()
+        except FileNotFoundError:
+            self.field = self.make_field(self.field_size)
+            self.ans = self.field
+        except EOFError:
+            self.field = self.make_field(self.field_size)
+            self.ans = self.field
+
+    def play(self):
+        for i in range(9):
+            print(self.field[i])
+        print('Enter square coordinate and square value')
+        inp = input().split()
+        if inp[0] == 'q':
+            del self.field
+            return 'q'
+        if self.field[int(inp[0]) - 1][int(inp[1]) - 1] != '_':
+            print('No, you can\'t do it like this. Try again')
+            return 0
+        else:
+            print('Ok')
+            self.field[int(inp[0]) - 1][int(inp[1]) - 1] = int(inp[2])
+            return 1
+
+    def check_ans(self):
+        if self.ans == self.field:
+            return 'Well done, my freind!'
+        else:
+            return 'Try better next time!'
 
 
 def run_func():
@@ -108,18 +154,39 @@ def run_func():
     if inp == 'me':
         print('Cool! Now enter the field size')
         fs = int(input())
-        player = PlayerSudoku('p', fs)
+        player = PlayerSudoku(fs)
         return player
     elif inp == 'computer':
         print('Great! Now enter the field size')
         fs = int(input())
-        comp = AISudoku('c', fs)
-        return comp
+        return AISudoku()
     else:
         print('No, it\'s just 2 variant. Try again')
         return run_func()
 
 
 if __name__ == '__main__':
-    c = run_func()
+    while True:
+        c = run_func()
+        k = 0
+        ans = ''
+        while True:
+            t = c.play()
+            if t == 'q':
+                c.save()
+                break
+            else:
+                k += t
+                if k == (81 - c.field_size):
+                    print(c.check_ans())
+                    print('Wanna play again? yes or no')
+                    ans = input()
+                    f = open('data.pkl', 'wb')
+                    pickle.dump('', f)
+                    f.close()
+                    break
+        if ans == 'yes':
+            continue
+        elif ans == 'no':
+            break
 
